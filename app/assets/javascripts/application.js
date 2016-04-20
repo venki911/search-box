a577b66fc3bcdb97c7e16ded9315021a24894f51
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2016-04-20 20:17:29 -0300

    organizing manifest files for angular components

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require angular
+//= require jquery
 //= require bootstrap
+//= require angular
+//= require angular-bootstrap
+//= require app.module
 //= require_tree .
