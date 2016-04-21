8573cb4dad8b8e4fca020fefb92ca4f3d9ac9e89
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2016-04-21 20:16:05 -0300

    adding Query model

---
+++ app/models/query.rb
@@ -0,0 +1,31 @@
+#
+# Query model for queries from users
+#
+# @author Marco Ávila <marcotulio.avila@gmail.com>
+class Query < ActiveRecord::Base
+  before_save :increment_counter, :downcase_text
+
+  scope :most_frequent, -> { order(count: :desc) }
+  scope :suggest_most_frequent, ->(text) { where('text like ?', "%#{text}%").most_frequent }
+
+  private
+    #
+    # Increments the query's counter on every save
+    #
+    # @author Marco Ávila <marcotulio.avila@gmail.com>
+    def increment_counter
+      unless self.count.nil?
+        self.count += 1
+      else
+        self.count = 1
+      end
+    end
+
+    #
+    # Make queries text downcase for better analysis
+    #
+    # @author Marco Ávila <marcotulio.avila@gmail.com>
+    def downcase_text
+      self.text.downcase!
+    end
+end
