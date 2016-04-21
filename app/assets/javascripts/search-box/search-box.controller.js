281dca7426a5ca476e847a2aae136f6d70f6244b
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2016-04-21 20:08:08 -0300

    improving and adding comments to component search-box
    It was added comments to directives, controllers and methods. It was
    also reindent the directive search-box.

--- app/assets/javascripts/search-box/search-box.controller.js
+++ app/assets/javascripts/search-box/search-box.controller.js
@@ -5,12 +5,57 @@
     .module('search-box')
     .controller('SearchBoxController', SearchBoxController);
 
-  SearchBoxController.$inject = ['$http'];
+  SearchBoxController.$inject = ['SearchService', '$scope'];
+
   /**
    * Controller for search-box directive
-   * @param {Object} $http Service
+   * @param {Object} SearchService Service
+   * @author Marco Ávila <marcotulio.avila@gmail.com>
    */
-  function SearchBoxController($http) {
-    console.log('controler');
+  function SearchBoxController(SearchService, $scope) {
+    var vm = this;
+    vm.query             = '';
+    vm.queries           = [];
+    vm.updateSuggestions = updateSuggestions;
+    vm.search            = search;
+    vm.selectQuery       = selectQuery;
+    vm.hideSuggestions   = true;
+
+    //////////////////
+
+    /**
+     * Updates the list of guessed queries
+     * @param  {string} text From user
+     * @return {void}
+     * @author Marco Ávila <marcotulio.avila@gmail.com>
+     */
+    function updateSuggestions(text) {
+      SearchService.getSuggestions(text).then(function(response) {
+        vm.queries = response.data;
+      });
+    }
+
+    /**
+     * Submit a query search
+     * @param  {string} query From user
+     * @return {void}
+     * @author Marco Ávila <marcotulio.avila@gmail.com>
+     */
+    function search(query) {
+      return SearchService.search(query);
+    }
+
+    /**
+     * Choose suggestion and search the selected query
+     * @param  {string} query Selected
+     * @return {void}
+     */
+    function selectQuery(query) {
+      vm.query = query.text;
+      vm.queries = [];
+      vm.search(vm.query);
+      $scope.$digest();
+      vm.hideSuggestions;
+    }
   }
 })();
--- app/assets/javascripts/search-box/search-box.js
+++ app/assets/javascripts/search-box/search-box.js
@@ -3,33 +3,49 @@
 
   angular
     .module('search-box', [
-        'ui.bootstrap'
+        'ui.bootstrap',
+        'ngAnimate',
     ])
     .directive('searchBox', searchBoxDirective);
 
-  ////////////////
-  // DIRECTIVES //
-  ////////////////
-
+  searchBoxDirective.$inject = ['SearchService'];
   /**
    * search-box directive
    * @return {void}
+   * @author Marco Ávila <marcotulio.avila@gmail.com>
    */
-  function searchBoxDirective() {
-      return {
-        templateUrl: 'search-box/search-box.tpl.html',
-        replace: true,
-        require: 'ngModel',
-        restrict: 'EA',
-        controller: 'SearchBoxController',
-        scope: {
-          ngModel: '='
-        },
-        link: function linkFunction(scope, element, attrs, ngModel) {
-          scope.$watch('ngModel', function(newValue) {
-            console.log('input value', newValue);
-          });
+  function searchBoxDirective(SearchService) {
+    return {
+      templateUrl: 'search-box/search-box.tpl.html',
+      replace: true,
+      restrict: 'E',
+      controller: 'SearchBoxController as vm',
+      scope: {},
+      link: linkFunction
+    };
+
+    /**
+     * Angular link function
+     * @param  {Object}              scope   Isolated Scope
+     * @param  {Object}              element DOM Element
+     * @param  {Array}               attrs   DOM Attributes
+     * @param  {SearchBoxController} vm      The search box controller
+     * @author Marco Ávila <marcotulio.avila@gmail.com>
+     * @return {void}
+     */
+    function linkFunction(scope, element, attrs, vm) {
+      scope.$watch('vm.query', function(newQuery, oldQuery) {
+        console.log('changed value', newQuery, oldQuery);
+        if (newQuery !== undefined) {
+          if (newQuery === '' || newQuery === scope.vm.queries[0]) {
+            return scope.vm.queries = [];
+          }
+          vm.hideSuggestions = false;
         }
-      };
+        if (newQuery !== oldQuery) {
+          vm.updateSuggestions(newQuery);
+        }
+      });
     }
+  }
 })();
