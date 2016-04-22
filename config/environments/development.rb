b0becfe4e1e7f7c37ff55d7f674eaf284f9cd03a
Marco Túlio de Ávila Santos <marco.santos@aker.com.br>
2016-04-22 09:44:09 -0300

    removing Rack::Lock as part of websocket-rails installation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Removing Rack::Lock middleware required by websocket-rails configuration
+  config.middleware.delete Rack::Lock
 end
